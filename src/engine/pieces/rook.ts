236f74f68bc9976fee94a8481a4c673517d94062
Maanav Chauhan <maanav.chauhan@softwire.com>
2026-09-11 16:52:48 +0100

    Rook moves laterally.

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
 
 export default class Rook extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,16 @@ export default class Rook extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let moves = new Array(0);
+        const piece = board.findPiece(this);
+        for (let i = 0; i < 8; i++) {
+            if (i !== piece.row) {
+                moves.push(Square.at(i, piece.col));
+            }
+            if (i !== piece.col) {
+                moves.push(Square.at(piece.row, i));
+            }
+        }
+        return moves;
     }
 }
