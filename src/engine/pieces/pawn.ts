2080408bbe04b284eb1891b4d5d9f951a3c0b257
Maanav Chauhan <maanav.chauhan@softwire.com>
2026-09-11 16:41:54 +0100

    Implemented Pawn Move.

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import piece from './piece';
+import Square from '../square';
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
@@ -8,6 +10,18 @@ export default class Pawn extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
+        const piece = board.findPiece(this);
+        if (this.player === Player.WHITE) {
+            if (board.getPiece(Square.at(piece.row + 1, piece.col)) === undefined) {
+                const oneSquareUp = Square.at(piece.row + 1, piece.col);
+                return [oneSquareUp];
+            }
+        } else {
+            if (board.getPiece(Square.at(piece.row - 1, piece.col)) === undefined) {
+                const oneSquareUp = Square.at(piece.row - 1, piece.col);
+                return [oneSquareUp];
+            }
+        }
         return new Array(0);
     }
 }
